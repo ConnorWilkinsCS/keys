c9b06185b40c700fd9de45cd41dd9bf8923a62ad
Connor Wilkins <connorwilkins2457@hotmail.com>
2023-09-20 17:11:36 -0400

    set up

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Reflection.Metadata;
 
+namespace keys
+{
+    class Program
+    {
+        //Create a data type which stores strings as “keys” and strings as “values”. Each key has on and only one value, and each value has one and only one key.
+        //Add functionality so a user can add data easily, remove data from the table, and retrieve a value given a key
+        public static void Main(string[] args)
+        {
+            //im not really sure how to start this question
+
+            // user can add data
+
+            // user can remove data
+
+            // retrieve value from a key
+        }
+        
+    }
+}
 
